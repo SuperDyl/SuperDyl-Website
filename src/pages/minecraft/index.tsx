dba0ed695c8dc157c0df54c1dcaabb6f5f7f55b3
Dylan <SuperDyl19@gmail.com>
2023-03-27 15:07:29 -0600

    moved RejectedChanges to its own page

--- src/pages/minecraft/index.tsx
+++ src/pages/minecraft/index.tsx
@@ -11,6 +11,7 @@ import News from "../../sections/News";
 import Rules from "../../sections/Rules";
 import Changes from "../../sections/Changes";
 import UnderReview from "../../sections/UnderReview";
+import RejectedChanges from "../../sections/RejectedChanges";
 
 const Minecraft = () => {
 	const email = useMemo(
@@ -44,39 +45,8 @@ const Minecraft = () => {
 			<UnderReview
 				underReviewHref={Forms.underReviewHref}
 				email={email}
-			></UnderReview>
-			<TitledSection header="Rejected Changes">
-				<Text>
-					These are (unless otherwise specified) my current stance against
-					changes I have rejected:
-				</Text>
-				<TitledSubSection header="Hack Clients">
-					<Text>Hack clients are not allowed</Text>
-				</TitledSubSection>
-				<TitledSubSection header="Operators, Moderators, Administrators">
-					<Text>
-						Operator status is not being given to other players. This decision
-						may be reviewed if later there are lots of players often using the
-						server and there are good players to trust with operator powers
-					</Text>
-					<Text>
-						Further, ownership of the server will not be switching hands at any
-						time. Running a server is not just having access to the admin
-						console, it requires keeping the server up to date, managing
-						backups, reviewing changes, and keeping peace between players. It
-						also costs money to run.
-					</Text>
-				</TitledSubSection>
-				<TitledSubSection header="Large Phantoms (Vanilla Tweaks)">
-					<Text>
-						Large Phantoms from Vanilla Tweaks will not be added anytime soon.
-						The main problem with the pack is that phantoms spawn for all
-						players in the world if at least one player {"hasn't"} slept for a
-						while. I {"don't"} want players that sleep consistently to be
-						punished for other players choosing to not sleep.
-					</Text>
-				</TitledSubSection>
-			</TitledSection>
+			/>
+			<RejectedChanges />
 			<TitledSection header="Datapacks">
 				<TitledSubSection header="Vanilla Tweaks">
 					<Text>
