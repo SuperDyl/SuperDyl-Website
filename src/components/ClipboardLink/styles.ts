8c1c7874407f65728adbfcb2bc63762788bdaaad
Dylan <SuperDyl19@gmail.com>
2023-06-05 00:32:47 -0600

    created a link to copy text to clipboard

---
+++ src/components/ClipboardLink/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { SharedLinkStyling } from "../SharedStyles";
+
+export const CopyToClipboard = styled.button`
+	${SharedLinkStyling}
+	position: relative;
+`;
