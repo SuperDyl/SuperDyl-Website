87c27db049a5e41fc8e91fe709a52cbdc7ffda3c
Dylan <SuperDyl19@gmail.com>
2023-06-30 21:18:37 -0600

    Simplified News page

--- src/pages/minecraft/news/index.tsx
+++ src/pages/minecraft/news/index.tsx
@@ -1,17 +1,14 @@
 import React, { memo, FunctionComponent } from "react";
 import Layout from "../../../components/Layout";
 import { MC_PAGES } from "../../../constants";
-import { news } from "../../../news";
-import NewsItem from "../../../components/NewsItem";
+import News from "../../../sections/News";
 
 interface NewsPageProps {}
 
 const NewsPage: FunctionComponent<NewsPageProps> = ({}) => {
 	return (
 		<Layout activeLink={MC_PAGES.NEWS}>
-			{news.map((newsItem) => (
-				<NewsItem news={newsItem} />
-			))}
+			<News />
 		</Layout>
 	);
 };
