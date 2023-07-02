2383bc560bc6b0bbcae017241925f411b7c562df
Dylan <SuperDyl19@gmail.com>
2023-07-02 09:54:52 -0600

    Added className option to icons

--- src/components/icons/AltRightArrow.tsx
+++ src/components/icons/AltRightArrow.tsx
@@ -1,13 +1,20 @@
-import React, { memo } from "react";
+import React, { FunctionComponent, memo } from "react";
 import { StyledSVG } from "../SharedStyles";
 
-const AltRightArrow = () => {
+interface AltRightArrowProps {
+	className?: string;
+}
+
+const AltRightArrow: FunctionComponent<AltRightArrowProps> = ({
+	className,
+}) => {
 	return (
 		<StyledSVG
 			xmlns="http://www.w3.org/2000/svg"
 			height="48"
 			viewBox="0 -1100 960 960"
 			width="48"
+			className={className}
 		>
 			<path d="m561-242-43-42 168-168H160v-60h526L517-681l43-42 241 241-240 240Z" />
 		</StyledSVG>
--- src/components/icons/ExpandLess.tsx
+++ src/components/icons/ExpandLess.tsx
@@ -1,13 +1,18 @@
-import React, { memo } from "react";
+import React, { FunctionComponent, memo } from "react";
 import { StyledSVG } from "../SharedStyles";
 
-const ExpandLess = () => {
+interface ExpandLessProps {
+	className?: string;
+}
+
+const ExpandLess: FunctionComponent<ExpandLessProps> = ({ className }) => {
 	return (
 		<StyledSVG
 			xmlns="http://www.w3.org/2000/svg"
 			height="48"
 			viewBox="0 -1100 960 960"
 			width="48"
+			className={className}
 		>
 			<path d="m283-345-43-43 240-240 240 239-43 43-197-197-197 198Z" />
 		</StyledSVG>
