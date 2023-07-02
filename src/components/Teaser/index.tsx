0bdac7659b9536f54f2e1c32a853ef5294cd4cdc
Dylan <SuperDyl19@gmail.com>
2023-07-02 09:14:51 -0600

    Created a Teaser component

---
+++ src/components/Teaser/index.tsx
@@ -0,0 +1,40 @@
+import React, { memo, FunctionComponent, ReactNode } from "react";
+import { HorizontalLine } from "../SharedStyles";
+import {
+	LinkContainer,
+	StyledExternalLink,
+	StyledLocalLink,
+	TeaserContainer,
+} from "./styles";
+
+interface TeaserProps {
+	className?: string;
+	children?: ReactNode;
+	url: string;
+	linkText: string;
+	isExternal?: boolean;
+}
+
+const Teaser: FunctionComponent<TeaserProps> = ({
+	className,
+	children,
+	url,
+	linkText,
+	isExternal = false,
+}) => {
+	return (
+		<TeaserContainer className={className}>
+			{children}
+			<HorizontalLine />
+			<LinkContainer>
+				{isExternal ? (
+					<StyledExternalLink href={url}>{linkText}</StyledExternalLink>
+				) : (
+					<StyledLocalLink to={url}>{linkText}</StyledLocalLink>
+				)}
+			</LinkContainer>
+		</TeaserContainer>
+	);
+};
+
+export default memo(Teaser);
---
+++ src/components/Teaser/styles.ts
@@ -0,0 +1,33 @@
+import NewsItem from "../NewsItem";
+import { ExternalLink, LocalLink } from "./../SharedStyles";
+import styled, { css } from "styled-components";
+
+export const TeaserContainer = styled.div`
+	background-color: ${(props) => props.theme.baseAlt};
+`;
+
+export const LinkContainer = styled.div`
+	padding: 1em;
+	display: flex;
+	justify-content: flex-end;
+	padding-top: 0.5em;
+`;
+
+const LinkStyling = css`
+	color: ${(props) => props.theme.primary};
+	font-size: 1.2em;
+	font-weight: bold;
+`;
+
+export const StyledLocalLink = styled(LocalLink)`
+	${LinkStyling}
+`;
+
+export const StyledExternalLink = styled(ExternalLink)`
+	${LinkStyling}
+`;
+
+export const StyledNewsItem = styled(NewsItem)`
+	margin: 0;
+	padding-bottom: 0.5em;
+`;
