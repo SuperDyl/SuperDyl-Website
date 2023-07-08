1be6a520bf84515fefb7e5af0a4123768fcbe2b1
Dylan <SuperDyl19@gmail.com>
2023-07-08 17:15:11 -0600

    TileGrid columnGroups can be overridden

--- src/components/TileGrid/index.tsx
+++ src/components/TileGrid/index.tsx
@@ -1,24 +1,26 @@
 import React, { memo, FunctionComponent, ReactNode, ReactElement } from "react";
 import { TileProps } from "../Tile";
-import { TileGridContainer } from "./styles";
+import { ColumnGroup, TileGridContainer } from "./styles";
 import { mobileWidth, tabletWidth } from "../../constants";
 
 interface TileGridProps {
 	className?: string;
 	children?: ReactElement<TileProps> | ReactElement<TileProps>[];
+	columnGroups?: ColumnGroup[];
 }
 
 const TileGrid: FunctionComponent<TileGridProps> = ({
 	className,
 	children,
+	columnGroups = [
+		{ minWidth: `${mobileWidth}px`, columns: 2 },
+		{ minWidth: `${tabletWidth}px`, columns: 4 },
+	],
 }) => {
 	return (
 		<TileGridContainer
 			className={className}
-			$columnGroups={[
-				{ minWidth: `${mobileWidth}px`, columns: 2 },
-				{ minWidth: `${tabletWidth}px`, columns: 4 },
-			]}
+			$columnGroups={columnGroups}
 		>
 			{children}
 		</TileGridContainer>
