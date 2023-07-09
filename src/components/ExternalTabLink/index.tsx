c5744edd93b8133ff3cf5c6731c64bea13ef11d0
Dylan <SuperDyl19@gmail.com>
2023-07-08 21:51:36 -0600

    Moved ExternalTabLink

--- src/components/ExternalTabLink/index.tsx
+++ src/components/ExternalTabLink/index.tsx
@@ -1,16 +1,18 @@
 import React, { memo, FunctionComponent, ReactNode } from "react";
-import { ExternalLink } from "./SharedStyles";
+import { ExternalLink } from "../SharedStyles";
 
 interface ExternalTabLinkProps {
 	className?: string;
 	children?: ReactNode;
 	href: string;
+	overrideParent?: boolean;
 }
 
 export const ExternalTabLink: FunctionComponent<ExternalTabLinkProps> = ({
 	children,
 	className,
 	href,
+	overrideParent,
 }) => {
 	return (
 		<ExternalLink
@@ -18,6 +20,15 @@ export const ExternalTabLink: FunctionComponent<ExternalTabLinkProps> = ({
 			href={href}
 			target="_blank"
 			rel="noopener noreferrer"
+			onClick={(e: React.FormEvent) => {
+				if (overrideParent) {
+					e.preventDefault();
+					const newTab = window.open(href, "_blank", "noopener,noreferrer");
+					if (newTab !== null) {
+						newTab.opener = null;
+					}
+				}
+			}}
 		>
 			{children}
 		</ExternalLink>
